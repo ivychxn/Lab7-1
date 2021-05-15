79f3297edf785c434e49de1ac3f46edf5c887c69
ivychxn <i1chan@ucsd.edu>
2021-05-15 04:53:11 -0700

    part 1

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,20 @@
 
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
+let pageNum = 0;
 
 // Make sure you register your service worker here too
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
@@ -12,7 +24,26 @@ document.addEventListener('DOMContentLoaded', () => {
       entries.forEach(entry => {
         let newPost = document.createElement('journal-entry');
         newPost.entry = entry;
+        pageNum++;
+        newPost.id = pageNum;
+        newPost.addEventListener('click', () => {
+          setState({name: 'entry', id: newPost.id});
+        });
         document.querySelector('main').appendChild(newPost);
       });
     });
+});
+
+let header = document.querySelector('header h1');
+header.addEventListener('click', () => {
+  setState({name: 'home'}, false);
+});
+
+let headerSettings = document.querySelector('header img');
+headerSettings.addEventListener('click', () => {
+  setState({name: 'settings'}, false);
+});
+
+window.addEventListener('popstate', (event) => {
+  setState(event.state, true);
 });
